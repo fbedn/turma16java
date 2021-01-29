fa25c9ad2a2d277db7d06490d1da6cc9c2df06e0
fbedn <f_ramos@outlook.com>
2021-01-29 11:54:30 -0300

    Nova funcionalidade de repetir 10x o loop no método da conta corrente

--- java/aulas/src/aulas/MiniSistemaBancario.java
+++ java/aulas/src/aulas/MiniSistemaBancario.java
@@ -71,54 +71,66 @@ public class MiniSistemaBancario {
 	
 	/***FUNÇÃO DO MENU CONTA CORRENTE***/
 	static double menuContaCorrente(Scanner ler, double saldo) {
-
-		System.out.println("SELECIONE O MOVIMENTO (DÉBITO OU CRÉDITO) - D/C:");
-		char movimento = ler.next().charAt(0);
-		System.out.println("VALOR:");
-		double valor = ler.nextDouble();
-		System.out.println("DESCRIÇÃO:");
-		String descricao = ler.next();
-		System.out.printf("\nSALDO ATUAL: R$ %.2f\n", saldo);
-		System.out.println("\nCONFIRMA A OPERAÇÃO? S/N:");
-		char continua = ler.next().charAt(0);
-		/* o código abaixo tentará realizar a operação de débito ou crédito */
-		if (continua == 's' || continua == 'S') {
-			if (movimento == 'd' || movimento == 'D') { // débito
-				if (saldo - valor >= 0) { // verifica se o saldo fica positivo
-					saldo = saldo - valor;
+		
+		int taloesEmitidos;
+		
+		for (int giro=1; giro<=10; giro++) {
+			System.out.println("SELECIONE O MOVIMENTO (DÉBITO OU CRÉDITO) - D/C:");
+			char movimento = ler.next().charAt(0);
+			System.out.println("VALOR:");
+			double valor = ler.nextDouble();
+			System.out.println("DESCRIÇÃO:");
+			String descricao = ler.next();
+			System.out.printf("\nSALDO ATUAL: R$ %.2f\n", saldo);
+			System.out.println("\nCONFIRMA A OPERAÇÃO? S/N:");
+			char continua = ler.next().charAt(0);
+			/* o código abaixo tentará realizar a operação de débito ou crédito */
+			if (continua == 's' || continua == 'S') {
+				if (movimento == 'd' || movimento == 'D') { // débito
+					if (saldo - valor >= 0) { // verifica se o saldo fica positivo
+						saldo = saldo - valor;
+						System.out.println("OPERAÇÃO REALIZADA COM SUCESSO!");
+					} else
+						System.out.println("\nNÃO FOI POSSÍVEL REALIZAR A OPERAÇÃO, POIS NÃO HÁ SALDO O SUFICIENTE...\nCANCELANDO OPERAÇÃO...");
+				} else if (movimento == 'c' || movimento == 'C') { // crédito
+					saldo = saldo + valor;
 					System.out.println("OPERAÇÃO REALIZADA COM SUCESSO!");
 				} else
-					System.out.println("\nNÃO FOI POSSÍVEL REALIZAR A OPERAÇÃO, POIS NÃO HÁ SALDO O SUFICIENTE...\nCANCELANDO OPERAÇÃO...");
-			} else if (movimento == 'c' || movimento == 'C') { // crédito
-				saldo = saldo + valor;
-				System.out.println("OPERAÇÃO REALIZADA COM SUCESSO!");
+					System.out.println("OPÇÃO " + movimento + " INVÁLIDA...\nCANCELANDO OPERAÇÃO...");
+	
+				System.out.printf("\nSALDO ATUAL: R$ %.2f\n", saldo);
 			} else
-				System.out.println("OPÇÃO " + movimento + " INVÁLIDA...\nCANCELANDO OPERAÇÃO...");
+				System.out.println("OPERAÇÃO CANCELADA...\n");
 
-			System.out.printf("\nSALDO ATUAL: R$ %.2f\n", saldo);
-		} else
-			System.out.println("OPERAÇÃO CANCELADA...\n");
-
-		//parte referente aos talões de cheque
-		System.out.println("\nDESEJA EMITIR TALÕES DE CHEQUE? S/N");
-		continua = ler.next().charAt(0);
-		if (continua == 's' || continua == 'S') {
-			int qtdTaloes;
-			do {
-				System.out.println("QUANTIDADE DE TALÕES DE CHEQUE:");
-				qtdTaloes = ler.nextInt();
-				if (qtdTaloes <= 0 || qtdTaloes > 3) {
-					System.out.println("VOCÊ PODE ESCOLHER ENTRE 1 E 3 TALÕES DE CHEQUE. INSERIR UMA OUTRA QUANTIDADE? S/N:");
-					continua = ler.next().charAt(0);
-					if (continua != 's' && continua != 'S') { // sai do loop se o usuário não responder sim à pergunta anterior
-						break;
+			//parte referente aos talões de cheque
+			System.out.println("\nDESEJA EMITIR TALÕES DE CHEQUE? S/N");
+			continua = ler.next().charAt(0);
+			if (continua == 's' || continua == 'S') {
+				int qtdTaloes;
+				do {
+					System.out.println("QUANTIDADE DE TALÕES DE CHEQUE:");
+					qtdTaloes = ler.nextInt();
+					if (qtdTaloes <= 0 || qtdTaloes > 3) {
+						System.out.println("VOCÊ PODE ESCOLHER ENTRE 1 E 3 TALÕES DE CHEQUE. INSERIR UMA OUTRA QUANTIDADE? S/N:");
+						continua = ler.next().charAt(0);
+						if (continua != 's' && continua != 'S') { // sai do loop se o usuário não responder sim à pergunta anterior
+							break;
+						}
 					}
-				}
-				if (qtdTaloes > 0 && qtdTaloes <= 3) { // se a quantidade de talões está dentro do aceitável
-					System.out.println("\nIMPRIMINDO TALÕES DE CHEQUE... ... ... FINALIZADO!");
-				}
-			} while (qtdTaloes < 0 || qtdTaloes > 3);
-
+					if (qtdTaloes > 0 && qtdTaloes <= 3) { // se a quantidade de talões está dentro do aceitável
+						System.out.println("\nIMPRIMINDO TALÕES DE CHEQUE... ... ... FINALIZADO!");
+					}
+				} while (qtdTaloes < 0 || qtdTaloes > 3);
+			}
+			
+			System.out.println("REALIZA OUTRA OPERAÇÃO NA SUA CONTA CORRENTE? S/N :");
+			continua = ler.next().charAt(0);
+			if (continua == 'N' || continua == 'n') {
+				System.out.printf("SALDO FINAL DA CONTA CORRENTE R$ %.2f \n", saldo);
+				System.out.println();
+				break;
+			}
+			
 		}
 		return saldo;
 	}
