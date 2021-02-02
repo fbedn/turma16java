62290d36a4e2179b0de98e93d5df61a6df4f636f
fbedn <f_ramos@outlook.com>
2021-02-02 10:59:13 -0300

    Adicionando a função da conta estudantil

--- java/aulas/src/aulas/MiniSistemaBancario.java
+++ java/aulas/src/aulas/MiniSistemaBancario.java
@@ -28,7 +28,7 @@ public class MiniSistemaBancario {
 			if (opcao == '1') {
 				
 				System.out.println("\nCONTA POUPANÇA");
-				//saldo = menuContaPoupanca(ler, saldo);
+				//menuContaPoupanca(ler);
 			}
 			else if (opcao == '2') {
 				
@@ -48,7 +48,7 @@ public class MiniSistemaBancario {
 			else if (opcao == '5') {
 				
 				System.out.println("\nCONTA ESTUDANTIL");
-				//menuContaEstudantil(ler);
+				menuContaEstudantil(ler);
 			}
 			else if (opcao == '6') {
 				
@@ -202,5 +202,86 @@ public class MiniSistemaBancario {
 		
 		System.out.println("Obrigada por usar nossos serviços!");
 		
+	}
+	
+	/*** FUNÇÃO DO MENU CONTA ESTUDANTIL ***/
+	static void menuContaEstudantil(Scanner ler) {
+		int tipo;
+		char mov, opc, ces, fim;
+		String desc;
+		double valor = 0.0, saldo = 0.0, saldoAtual = 0.0, credito = 0.0, debito = 0.0;
+		double emprestimo = 0.0, valorEmprestimo = 0.00, saldoFinal = 0.00;
+		char opcao;
+
+		System.out.print("\nVocê selecionou a Conta Estudandil");
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.printf("\n%d°Consulta: Escolha D-Débito/C-Crédito", i);
+			mov = ler.next().toUpperCase().charAt(0);
+			if (mov == 'D') {
+				System.out.printf("\nDigite o valor para débito: ");
+				debito = ler.nextDouble();
+				if (saldo > 0) {
+					while (saldo < debito) {
+						System.out.printf("\nSaldo indisponivel, atual %.2f, tente de novo!!!\n", saldo);
+						System.out.print("\nDigite o valor para débito :");
+						debito = ler.nextDouble();
+					}
+					ler.nextLine();
+					System.out.print("\nDescrição do débito :");
+					desc = ler.nextLine();
+					saldo = saldo - debito;
+					System.out.printf("\nSaldo atual é R$ %.2f\n", saldo);
+				} else if (saldo == 0) {
+					System.out.println("\n0 saldo, faça um crédito primeiro!!!");
+				}
+			} else if (mov == 'C') {
+				System.out.print("\nDigite o valor do crédito: ");
+				credito = ler.nextDouble();
+				while (credito <= 0) {
+					System.out.print("\nValor informado negativo ou zero, tente de novo:");
+					System.out.print("\nDigite o valor do crédito: ");
+					credito = ler.nextDouble();
+				}
+				ler.nextLine();
+				System.out.print("\nDigite a descrição do crédito: ");
+				desc = ler.nextLine();
+				saldo = saldo + credito;
+				System.out.printf("\nSaldo atual é R$ %.2f \n", saldo);
+			} else {
+				System.out.println("\nVocê não escolheu Debito ou Crédito!!!");
+			}
+
+			System.out.print("\nContinua S/N :");
+			fim = ler.next().toUpperCase().charAt(0);
+			if (fim == 'N') {
+				break;
+			}
+		}
+		System.out.printf("\nDESEJA USAR O LIMITE DE ESTUDANTE - S/N:");
+		System.out.printf("\nSE SIM ELE TE PODE USAR 5 MIL: ");
+		opcao = ler.next().toUpperCase().charAt(0);
+
+		do
+		{
+
+			System.out.printf("\nInsira o valor do Empréstimo:");
+			emprestimo = ler.nextDouble();
+			if (emprestimo < 5000) {
+				valorEmprestimo = emprestimo;
+
+			} else {
+				System.out.printf("\nValor Inválido");
+			}
+
+			saldoFinal = valorEmprestimo + saldo;
+			System.out.printf("\nO saldo final da sua conta é: %.2f\n", saldoFinal);
+
+			System.out.printf("\nDESEJA CONTINUAR?- S/N: ");
+			opcao = ler.next().toUpperCase().charAt(0);
+		} while (opcao == 'S');
+
+		System.out.printf("\nOBRIGADO POR UTILIZAR NOSSOS SERVIÇOS ");
+
 	}
 }
